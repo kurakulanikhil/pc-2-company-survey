22f8e027703e6570aebfd7550eb3d080d9deeb8e
nikhi <nikhildhoni17@gmail.com>
2022-08-23 20:30:26 -0500

    completed

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -5,16 +5,35 @@ public class AverageAge {
     public String averageAgeCalculator(String[] ageRawData)
     {
         double avg = 0;
+        double sum = 0;
 
-        try{
+        try {
             //Write the logic
+            for(int i = 0;i< ageRawData.length;i++){
+             // int sum = Double.parseDouble(sum + ageRawData[i]);
+                 sum = sum + Integer.parseInt( ageRawData[i]);
 
+
+          }
+            avg = sum/ ageRawData.length;
+
+            return String.valueOf(avg);
             //Handle specific exception
         }catch (Exception exception)
         {
             return exception.toString();
         }
-        return String.valueOf(avg);
+        //return String.valueOf(avg);
+    }
+
+    public static void main(String[] args) {
+        String[] age = {"35","26","32","44"};
+        AverageAge obj = new AverageAge();
+        System.out.println(obj.averageAgeCalculator(age));
+        String[] age1 = {"35","26","thirty two","44"};
+
+        System.out.println(obj.averageAgeCalculator(age1));
+
     }
 
 
